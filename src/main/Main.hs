eda0d372b7de99055f7451997b88407035877dd2
Chris Done <chrisdone@gmail.com>
2014-06-21 01:01:30 +0200

    Help screen

--- src/main/Main.hs
+++ src/main/Main.hs
@@ -1,53 +1,80 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE ViewPatterns #-}
 -- | Main command-line interface.
 
 module Main where
 
-import Haskell.Docs
-import Haskell.Docs.Types
-import Haskell.Docs.Ghc
+import           Haskell.Docs
+import           Haskell.Docs.Ghc
+import           Haskell.Docs.Types
 
+import           Control.Exception
+import           Control.Exception (IOException)
 import qualified Control.Exception as E
-import System.Environment
+import           GHC
+import           GhcMonad
+import           System.Environment
+import           System.Exit
+import           System.IO
 
 -- | Main entry point.
 main :: IO ()
 main =
   do args <- getArgs
-     E.catch
-       (app args)
-       (error . printEx)
+     app args
 
 -- | Do the printing.
 app :: [String] -> IO ()
 app (extract -> (gs,ms,as)) =
   withInitializedPackages
     gs
-    (case as of
-       [name] ->
-         searchAndPrintDoc ms
-                           Nothing
-                           Nothing
-                           (Identifier name)
-       [mname,name,pname] ->
-         searchAndPrintDoc ms
-                           (Just (PackageName pname))
-                           (Just (makeModuleName mname))
-                           (Identifier name)
-       [mname,name] ->
-         searchAndPrintDoc ms
-                           Nothing
-                           (Just (makeModuleName mname))
-                           (Identifier name)
-       _ -> error "<module-name> <ident> [<package-name>] | <ident>")
+    (catchErrors
+       (case as of
+          [name] ->
+            searchAndPrintDoc ms
+                              Nothing
+                              Nothing
+                              (Identifier name)
+          [mname,name,pname] ->
+            searchAndPrintDoc ms
+                              (Just (PackageName pname))
+                              (Just (makeModuleName mname))
+                              (Identifier name)
+          [mname,name] ->
+            searchAndPrintDoc ms
+                              Nothing
+                              (Just (makeModuleName mname))
+                              (Identifier name)
+          _ -> bail "<module-name> <ident> [<package-name>] | <ident>\n\
+                    \\n\
+                    \Options: --g <ghc option> Specify GHC options.\n\
+                    \         --sexp           Output s-expressions.\n\
+                    \         --modules        Only output modules."))
 
 -- | Extract arguments.
 extract :: [String] -> ([String],Bool,[String])
 extract = go ([],False,[])
-  where go (gs,ms,as) ("-g":arg:ys)    = go (arg:gs,ms,as) ys
-        go (gs,ms,as) ("--modules":ys) = go (gs,True,as) ys
-        go (gs,ms,as) (y:ys)           = go (gs,ms,y:as) ys
-        go (gs,ms,as) []               = (gs,ms,as)
+  where
+    go (gs,ms,as) ("-g":arg:ys)    = go (arg:gs,ms,as) ys
+    go (gs,ms,as) ("--modules":ys) = go (gs,True,as) ys
+    go (gs,ms,as) ("--sexp":ys)    = go (gs,ms,as) ys
+    go (gs,ms,as) (y:ys)           = go (gs,ms,y:as) ys
+    go (gs,ms,as) []               = (gs,ms,as)
+
+-- | Catch errors and print 'em out.
+catchErrors :: Ghc () -> Ghc ()
+catchErrors m =
+  gcatch (gcatch m
+                 (\x ->
+                    do bail (printEx x)
+                       liftIO exitFailure))
+         (\(e::SomeException) ->
+            bail (show e))
+
+-- | Print an error and bail out.
+bail :: String -> Ghc ()
+bail e =
+  liftIO (hPutStrLn stderr e)
 
 -- | Print an exception for humans.
 printEx :: DocsException -> String
